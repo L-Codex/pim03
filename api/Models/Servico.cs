ffd9179f78b9614114cc4a9f19314ed516d185c3
Leonardo <37600692+bpleonardo@users.noreply.github.com>
2026-04-26 22:08:21 -0300

    feat: adicionar campo Id no modelo Service

--- api/Models/Servico.cs
+++ api/Models/Servico.cs
@@ -1,9 +1,14 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using api.Utilities;
 
 namespace api.Models
 {
     public class Servico
     {
+        [Required]
+        [Guid]
+        public string Id { get; init; }
+
         [Required]
         [MinLength(3, ErrorMessage = "{0} deve conter pelo menos {1} caracteres!")]
         public string Nome { get; set; }
@@ -15,14 +20,25 @@ namespace api.Models
         [Range(0.01, double.MaxValue, ErrorMessage = "{0} deve ser um valor positivo!")]
         public double Preco { get; set; }
 
-        public Servico(string nome, string? descricao, double preco)
+        public Servico(string? id, string nome, string? descricao, double preco)
         {
+            if (id == null)
+            {
+                Id = Guid.NewGuid().ToString();
+            }
+            else
+            {
+                Id = id;
+            }
             Nome = nome;
             Descricao = descricao;
             Preco = preco;
         }
 
+        public Servico(string id, string nome, double preco)
+            : this(id, nome, null, preco) { }
+
         public Servico(string nome, double preco)
-            : this(nome, null, preco) { }
+            : this(null, nome, null, preco) { }
     }
 }
